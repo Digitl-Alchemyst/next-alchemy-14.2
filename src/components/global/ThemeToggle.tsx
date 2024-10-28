837311695a878b37c6663a50fa7627673fd7263f
Digitl Alchemyst <129696730+Digitl-Alchemyst@users.noreply.github.com>
2024-10-28 15:03:26 -0500

    Dark and Light mode for button

--- src/components/global/ThemeToggle.tsx
+++ src/components/global/ThemeToggle.tsx
@@ -26,21 +26,24 @@ export function ThemeToggle() {
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end' className='space-y-3 border-none bg-dark-400 px-2 py-3'>
+      <DropdownMenuContent
+        align='end'
+        className='space-y-3 border-none bg-light-200 px-3 py-4 dark:bg-dark-700'
+      >
         <DropdownMenuItem
-          className='border border-dark-200 bg-dark-600 px-2 py-3'
+          className='border border-light-400 bg-light-100 px-3 py-3 text-dark-700 hover:border-accent3-300 hover:bg-accent3-200 hover:text-accent3-800 dark:border-dark-400 dark:bg-dark-600 dark:text-light-200 dark:hover:border-accent1-500 dark:hover:bg-accent1-700 dark:hover:text-accent1-100'
           onClick={() => setTheme('light')}
         >
           Light
         </DropdownMenuItem>
         <DropdownMenuItem
-          className='border border-dark-200 bg-dark-600 px-2 py-3'
+          className='border border-light-400 bg-light-100 px-3 py-3 text-dark-700 hover:border-accent4-300 hover:bg-accent4-200 hover:text-accent4-900 dark:border-dark-400 dark:bg-dark-600 dark:text-light-200 dark:hover:border-accent2-500 dark:hover:bg-accent2-700 dark:hover:text-accent2-100'
           onClick={() => setTheme('dark')}
         >
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem
-          className='border border-dark-200 bg-dark-600 px-2 py-3'
+          className='border border-light-400 bg-light-100 px-3 py-3 text-dark-700 hover:border-accent1-300 hover:bg-accent1-200 hover:text-accent1-800 dark:border-dark-400 dark:bg-dark-600 dark:text-light-200 dark:hover:border-accent3-600 dark:hover:bg-accent3-800 dark:hover:text-accent3-100'
           onClick={() => setTheme('system')}
         >
           System
